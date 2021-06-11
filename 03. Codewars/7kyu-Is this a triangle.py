91b69fddb80aae59ba1d8dae8b2457ece3a1046f
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-06-11 23:58:16 +0000

    for best practice! more clever way

--- 03. Codewars/7kyu-Is this a triangle.py
+++ 03. Codewars/7kyu-Is this a triangle.py
@@ -2,6 +2,7 @@
 
 # the longest line should shorter than other lines
 
+"""
 def is_triangle(a, b, c):
     lst = sorted([a, b, c], reverse=True)
     max_len = lst[0]
@@ -10,3 +11,9 @@ def is_triangle(a, b, c):
     if max_len < sum(others):
         return True
     return False
+"""
+
+# Update - more clever
+def is_triangle(a, b, c):
+  a, b, c = sorted([a, b, c], reverse=True)
+  return b+c > a
