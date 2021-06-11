1beb83dca80657a0e2b1f8675e9bda1e92262f79
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-06-11 00:34:20 +0000

    is this a triangle?

---
+++ 03. Codewars/7kyu-Is this a triangle.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/56606694ec01347ce800001b/train/python
+
+# the longest line should shorter than other lines
+
+def is_triangle(a, b, c):
+    lst = sorted([a, b, c], reverse=True)
+    max_len = lst[0]
+    others = lst[1:]
+#     print(max_len, others)
+    if max_len < sum(others):
+        return True
+    return False
