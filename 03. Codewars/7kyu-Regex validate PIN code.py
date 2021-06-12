0f7d56f970d16d8d605a3b93815c15a1f55abec0
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-06-12 23:00:55 +0000

    Regex validate PIN code using isdigit() mehtod

--- 03. Codewars/7kyu-Regex validate PIN code.py
+++ 03. Codewars/7kyu-Regex validate PIN code.py
@@ -1,19 +1,19 @@
+# 
+# First Trial
 def validate_pin(pin):
     #return true or false
-    
-    if len(pin) == 4 or len(pin) == 6:
+    if len(pin) in [4, 6]:
         for p in pin:
-            if p in '0123456789':
-                continue
-            else:
+            if p not in "0123456789":
                 return False
-            
         return True
     
     else:
         return False
-    
-    
-# upgrade - much simple!
+
+# Using .isdigit()
+# https://www.w3schools.com/python/ref_string_isdigit.asp
+# python isdigit() method return True is all the characters in text are digits
 def validate_pin(pin):
+    #return true or false
     return len(pin) in [4, 6] and pin.isdigit()
