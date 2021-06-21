01def228b24713780a5e22887359bbb716945550
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-06-21 12:17:07 +0000

    :bookmark: formating

--- 03. Codewars/Fundamentals/7kyu-Printer Errors.py
+++ 03. Codewars/Fundamentals/7kyu-Printer Errors.py
@@ -2,3 +2,11 @@ def printer_error(s):
     # your code
     ext = "nopqrstuvwxyz"
     return str(sum(1 for e in s.lower() if e in ext))+"/"+str(len(s))
+
+
+# Refactoring
+# formating
+def printer_error(s):
+    # your code
+    ext = "nopqrstuvwxyz"
+    return "{}/{}".format(str(sum(1 for e in s.lower() if e in ext)), len(s))  # formating
