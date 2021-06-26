a364734313c3102e957d27137a948765d8ee942f
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-06-26 23:53:42 +0000

    :tada: refactored it! -21.06.27.sun. am 8:53-

--- 03. Codewars/Fundamentals/7kyu-Largest 5 digit numbers in a Series.py
+++ 03. Codewars/Fundamentals/7kyu-Largest 5 digit numbers in a Series.py
@@ -12,3 +12,11 @@ def solution(digits):
         lst.append(int(digits[i:i+5]))
     
     return max(lst)
+
+
+
+# refactoring it! -21.06.27.sum. am 8:52-
+# simple & bueauty
+
+  def solution(digits):
+        return max(int(digits[i: i+5]) for i in range(0, len(digits)-4))
