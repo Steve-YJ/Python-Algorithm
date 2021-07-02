fcdb36522b0c49e32e1263f7e17fcee7863bda03
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-02 12:38:04 +0000

    :bookmark: passed test case, but need more...! -21.07.02.Fri.pm9:37-

--- 03. Codewars/Fundamentals/7kyu-A Rule of Divisibility by 7.py
+++ 03. Codewars/Fundamentals/7kyu-A Rule of Divisibility by 7.py
@@ -1,28 +1,17 @@
-def step_func(m):
-    x = m//10
-    y = m%10
-    return x-(2*y)
-def divisible(m, step):
-    if len(str(m)) <=2 and step_func(m) % 7 == 0:
-        return 1
-    elif len(str(m)) >= 3 and step_func(m) % 7 == 0:
-        return divisible(m, step)
-
-
 def seven(m):
-    step = 0
-    print("print_m: {}".format(m))
     # your code
-    if divisible(m, step) == 1:
-        return (m, step)
-    else:
-        step +=1
-        return divisible(m, step)
-    
+    step = 0
+        
+    while True:
+        print(m)
+        if len(str(m)) <= 2 and m % 7 == 0:
+            return (m, step)
+        else:
+            x, y = m//10, m%10
+            if (x-(2*y)) % 7 == 0:
+                m = x-(2*y)
+                step += 1
+            else:
+                return
 
-    
-#     if len(str(m)) <= 2 and m%7==0:
-#         return (m, step)
-#     else:
-#         step +=1
-#         return seven(step_func(m))
+        
