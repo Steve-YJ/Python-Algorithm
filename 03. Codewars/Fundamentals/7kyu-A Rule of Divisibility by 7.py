cd670cff686f611ef130a4d9c04b97e7006bd700
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-02 12:52:31 +0000

    :boomark: solved it!...! but......but.... -21.07.02.fri-

--- 03. Codewars/Fundamentals/7kyu-A Rule of Divisibility by 7.py
+++ 03. Codewars/Fundamentals/7kyu-A Rule of Divisibility by 7.py
@@ -3,15 +3,18 @@ def seven(m):
     step = 0
         
     while True:
-        print(m)
         if len(str(m)) <= 2 and m % 7 == 0:
             return (m, step)
+        elif len(str(m)) <= 2:
+            return (m, step)
+        
         else:
             x, y = m//10, m%10
-            if (x-(2*y)) % 7 == 0:
+            if len(str(m)) >= 3 and (x-(2*y)) % 7 == 0:
                 m = x-(2*y)
                 step += 1
             else:
-                return
-
-        
+                m = x-(2*y)
+                step += 1
+            
+            
