de2db4ba0d28148ac6ada7698d32d619b514a7ed
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-10 09:48:02 +0000

    :bookmark: refactored it! -21.07.10.Sat-

--- 03. Codewars/Fundamentals/7kyu-Speed Control.py
+++ 03. Codewars/Fundamentals/7kyu-Speed Control.py
@@ -1,14 +1,10 @@
 def gps(s, x):
-#     print("print s: {}".format(s))
-#     print("print x: {}".format(x))
-    
     # your code
     if len(x) <= 1:
         return 0
     else:
         lst = [x[i+1]-x[i] for i in range(len(x)-1)]
-        avg_h = [3600 * e for e in lst]
-#         print("print 3600 * lst: {}".format(3600 * lst))  # fixed bug
-        avg_h = [e/s for e in avg_h]
-        
-        return round(max(avg_h), 2)
+        avg_h = [(3600 * e)/s for e in lst]
+        return int(max(avg_h))
+
+
