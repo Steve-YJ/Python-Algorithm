ebf5d78f6236b1b673f2b98de418eeaf4802e9f9
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-15 23:49:13 +0000

    :bookmark: Max Buffer Size Reached -21.07.16.Fri-

---
+++ 03. Codewars/5kyu/Subsequence Sums.py
@@ -0,0 +1,17 @@
+"""
+Max Buffer Size Reached (1.5 MiB) -21.07.16.Fri.am8:48-
+"""
+
+def subsequence_sums(arr, s):
+    # TODO - your code here
+    count = 0  # subsequence Sums
+    
+    for i in range(1, len(arr)+1):
+        for j in range(0, len(arr)-(i-1), 1):
+            if sum(arr[j:j+i]) == s:
+                count += 1
+                
+    return count
+            
+        
+        
