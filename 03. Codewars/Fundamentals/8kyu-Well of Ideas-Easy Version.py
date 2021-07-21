b41069f62a5afc7e5e84d3abd96c732baca853b4
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-21 12:18:12 +0000

    :bookmark: refactoring!

--- 03. Codewars/Fundamentals/8kyu-Well of Ideas-Easy Version.py
+++ 03. Codewars/Fundamentals/8kyu-Well of Ideas-Easy Version.py
@@ -1,8 +1,8 @@
 def well(x):
     #your code here
-    if "good" not in x:
-        return "Fail!"
-    elif x.count("good") in [1, 2]:
-        return "Publish!"
-    elif x.count("good")>2:
+    if x.count("good")>2:
         return "I smell a series!"
+    if "good" in x:
+        return "Publish!"
+    else:
+        return "Fail!"
