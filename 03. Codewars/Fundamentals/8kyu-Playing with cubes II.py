ccbad26d6558fa3d13f5e614061dca5d1a5a550a
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-22 22:06:55 +0000

    solved! keep going!

---
+++ 03. Codewars/Fundamentals/8kyu-Playing with cubes II.py
@@ -0,0 +1,13 @@
+class Cube(object):
+    # This cube needs help
+    # Define a constructor which takes one integer, or handles no args
+    def __init__(self, integer=0):
+        self._side=integer
+        
+    def get_side(self):
+        """Return the side of the Cube"""
+        return abs(self._side)
+
+    def set_side(self, new_side):
+        """Set the value of the Cube's side."""
+        self._side = new_side
