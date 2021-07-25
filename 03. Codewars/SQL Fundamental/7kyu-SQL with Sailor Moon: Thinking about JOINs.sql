4e05689de14015e9eb9f027b815117a2abbc553e
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-25 02:53:56 +0000

    :bookmark: much clear!

--- 03. Codewars/SQL Fundamental/7kyu-SQL with Sailor Moon: Thinking about JOINs.sql
+++ 03. Codewars/SQL Fundamental/7kyu-SQL with Sailor Moon: Thinking about JOINs.sql
@@ -1,5 +1,10 @@
 --your code here--
-SELECT sailorsenshi.senshi_name AS sailor_senshi, sailorsenshi.real_name_jpn as real_name, cats.name AS cat, schools.school AS school
-FROM sailorsenshi
-LEFT JOIN cats ON cats.id = sailorsenshi.cat_id
-LEFT JOIN schools ON schools.id=sailorsenshi.school_id
+SELECT
+  ss.senshi_name AS sailor_senshi,
+  ss.real_name_jpn AS real_name,
+  ca.name AS cat,
+  sc.school AS school
+  
+FROM sailorsenshi as ss
+LEFT JOIN cats as ca ON cat_id = ca.id
+LEFT JOIN schools as sc ON school_id = sc.id
