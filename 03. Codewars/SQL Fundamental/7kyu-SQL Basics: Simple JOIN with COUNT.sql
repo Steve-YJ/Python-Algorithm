4ec08a3e9d30fe08541bd63c3c8cd9416e8139d0
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-07-25 01:02:28 +0000

    :bookmark: need to practice SQL

--- 03. Codewars/SQL Fundamental/7kyu-SQL Basics: Simple JOIN with COUNT.sql
+++ 03. Codewars/SQL Fundamental/7kyu-SQL Basics: Simple JOIN with COUNT.sql
@@ -1,6 +1,6 @@
 -- Create your SELECT statement here
 
-SELECT people.id, people.name, count(*) AS toy_count
+SELECT people.id, people.name, COUNT(people.id) AS toy_count
 FROM people
 LEFT JOIN toys ON people.id = toys.people_id
 GROUP BY people.id
