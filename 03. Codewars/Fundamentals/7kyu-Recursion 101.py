256b4218d9501207fb8fcd063ba1812763783fe8
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-08-07 14:06:34 +0000

    :bookmark: passed test case, but failed to submit

---
+++ 03. Codewars/Fundamentals/7kyu-Recursion 101.py
@@ -0,0 +1,33 @@
+def step1(a,b):
+    # print("step1")
+    # print(a,b)
+    if a==0 or b==0:
+        return [a,b]
+    else:
+        return step2(a,b)
+
+def step2(a,b):
+    # print("step2")
+    # print(a,b)
+    if a>=2*b:
+        a=a-2*b
+        return step1(a,b)
+    else:
+        return step3(a,b)
+
+def step3(a,b):
+    # print("step3")
+    # print(a,b)
+    if b>=2*a:
+        b=b-2*a
+        return step1(a,b)
+    else:
+        return [a,b]
+
+def solve(a,b):
+    return step1(a,b)
+
+        
+    
+            
+    
