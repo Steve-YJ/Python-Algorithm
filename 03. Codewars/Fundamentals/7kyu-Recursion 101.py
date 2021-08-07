6f845b802dc4dd5622fd8b19aafb42c0ca842a65
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-08-07 14:19:21 +0000

    :bookmark: recursion error happened. So I need to fix it!

--- 03. Codewars/Fundamentals/7kyu-Recursion 101.py
+++ 03. Codewars/Fundamentals/7kyu-Recursion 101.py
@@ -1,14 +1,12 @@
+# RecursionError: maximum recursion depth exceeded in comparison
+
 def step1(a,b):
-    # print("step1")
-    # print(a,b)
     if a==0 or b==0:
         return [a,b]
     else:
         return step2(a,b)
 
 def step2(a,b):
-    # print("step2")
-    # print(a,b)
     if a>=2*b:
         a=a-2*b
         return step1(a,b)
@@ -16,8 +14,6 @@ def step2(a,b):
         return step3(a,b)
 
 def step3(a,b):
-    # print("step3")
-    # print(a,b)
     if b>=2*a:
         b=b-2*a
         return step1(a,b)
@@ -26,8 +22,4 @@ def step3(a,b):
 
 def solve(a,b):
     return step1(a,b)
-
-        
-    
-            
     
