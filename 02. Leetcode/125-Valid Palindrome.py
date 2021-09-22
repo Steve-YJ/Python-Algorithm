8c4f0af8412b4857897c556ab48295df6946976c
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2021-09-22 22:14:24 +0900

    Update 125-Valid Palindrome.py
    Using Deque Data Structure
    Restart! Keep going!

--- 02. Leetcode/125-Valid Palindrome.py
+++ 02. Leetcode/125-Valid Palindrome.py
@@ -22,3 +22,23 @@ class Solution:
                 return False
             
         return True
+    
+    
+# Using Deque
+import collections
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # using deque data structure
+        strs: Deque = collections.deque()
+            
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+                
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+            
+        return True
