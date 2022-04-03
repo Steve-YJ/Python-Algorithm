6c4d0f4860d5a5419ae6ba1083ef768a6e1c3b68
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2022-04-03 12:28:29 +0000

    :tada: Solved!
    Learned number & loop

--- 05. BOJ(beakjoon_online_judge)/step_by_step/1110.py
+++ 05. BOJ(beakjoon_online_judge)/step_by_step/1110.py
@@ -1,30 +1,23 @@
-origin = int(input())  # get nuber
-temp = origin
-sum_ = 0
-new = 100
-count = 0 # count from 0
+n = int(input())  # 0 <= n <= 99
+origin = n
+temp = 100
+count = 0
 
 while True:
-    if count != 0 and temp == origin:  # if temp == origin then return count
+    if temp == origin:
         print(count)
         break
-    else:
-        if temp >= 10:
-            temp = str(temp)
-            first, second = temp[0], temp[1]
-            sum_ = int(first) + int(second)
-            new = second + str(sum_)[-1] # 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리수를 이어 붙여 새로운 수를 만들어 준다
 
-            # change data type again
-            # from string to int
-            temp = int(temp)
-            new = int(new)
-            # add count
-            count += 1
-            # now new become temp
-            temp = new
+    else:
+        # if count == 0 then temp = origin
+        if count == 0:
+            temp = origin
             
-        else:  # if temp < 10
-            new = str(temp) + str(temp)
-            count += 1
-             
+        # 주어진 자리수를 구해준다
+        left, right = temp // 10, temp % 10
+        # 주어진 자리수의 합을 구해준다
+        sum_ = left + right
+        # ㅈ두어진 수의 가장 오른쪽값과 합의 가장 오른쪽 자리수를 이어붙여 새 수를 만들어준다
+        temp = str(right) + str(sum_ % 10)
+        temp = int(temp)
+        count += 1
