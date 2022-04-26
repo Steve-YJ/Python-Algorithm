caca8fc18c3d2252bad221fc1d24e1ff60346081
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2022-04-26 00:07:21 +0000

    :memo: Solved!

--- 05. BOJ(beakjoon_online_judge)/step_by_step/1032.py
+++ 05. BOJ(beakjoon_online_judge)/step_by_step/1032.py
@@ -1,21 +1,26 @@
-# 3
-# config.sys
-# config.inf
-# configures
-
+"""
+파일을 입력받고 파일명을 하나씩 비교하는 아이디어
+- 파일명이 다를경우, ?로 일관한다!
+"""
 n = int(input())
 files = []
+# result = []
 
 for i in range(n):
     files.append(str(input()))
 
-data = [s for s in files[i]]
+# set the baseline to data
+data = [s for s in files[0]]
 result = data
 
 for i in range(1, n):
     file = files[i]
     for j in range(len(file)):
+        # print(f"file[j]: {file[j]}")
+        # print(f"data[j]: {data[j]}")
         if file[j] != data[j]:
+            # print(file[j])
+            # print(data[j])
             result[j] = ("?")
-
-print("".join(s for s in result))          
+# print(result)
+print("".join(s for s in result))
