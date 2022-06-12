f11f0d2e2ee6f50510653a7719d08be70ecc7306
Youngjeonlee <45029428+Steve-YJ@users.noreply.github.com>
2022-06-12 14:23:27 +0000

    :memo: edit the code -2022.06.12.sun-
    * changed the variable: map -> maps
    * changed variable 'maps' as 2D-array
    * set the x, y

--- 01. 이코테(이것이 취업을 위한 코딩테스트이다)/Ch04. 구현/example_4.1_상하좌우.py
+++ 01. 이코테(이것이 취업을 위한 코딩테스트이다)/Ch04. 구현/example_4.1_상하좌우.py
@@ -1,31 +1,31 @@
-n = int(input("input number: "))
+n = int(input("input n: "))
+# makes maps
+maps = [[(i + 1, j + 1) for j in range(n)] for i in range(n)]
+
+# get the move
 move = list(map(str, input().split()))
-map = [(i + 1, j + 1) for j in range(n) for i in range(n)]
 
-cur = [1, 1]
-print(f"starting_point: {cur}")
+# print(f"n: {n}")
+# print(f"maps: {maps}")
+# print(f"move: {move}")
 
+# set the current location
+cur = maps[0][0]
+# print(f"cur: {cur}")
 
+# set the x, y 
+x, y = cur[0], cur[1]
+
+# let's move!
 for m in move:
-    if m == 'R':
-    # pass
-    # To-do
-    if cur[1] < n:
-        cur[1] += 1
-  
-    elif m == 'L':
-    # pass
-    if cur[1] > 1:
-        cur[1] -= 1
-  
-    elif m == 'D':
-    # pass
-        if cur[0] < n:
-        cur[0] += 1
-    
-    elif m == 'U':
-    # pass
-    if cur[0] > 1:
-        cur[0] -= 1
+    if m == "R" and y < n:
+        y += 1
+    elif m == "L" and y > 1:
+        y -= 1
+    elif m == "U" and x > 1 :
+        x -= 1
+    elif m == "D" and x < n:
+        x += 1
 
-print(cur[0], cur[1])
+# print results
+print(x, y)
