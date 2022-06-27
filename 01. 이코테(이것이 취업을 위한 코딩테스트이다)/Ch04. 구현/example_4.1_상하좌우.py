e38aacade08cd95cd000d01a94639c414d162a2f
Steve Young-Jeon Lee <45029428+Steve-YJ@users.noreply.github.com>
2022-06-28 07:10:00 +0900

    Update example_4.1_상하좌우.py
    📝  Got the result but I want to optimize the code
    
    * How can I optimize the code below?

--- 01. 이코테(이것이 취업을 위한 코딩테스트이다)/Ch04. 구현/example_4.1_상하좌우.py
+++ 01. 이코테(이것이 취업을 위한 코딩테스트이다)/Ch04. 구현/example_4.1_상하좌우.py
@@ -1,31 +1,39 @@
-n = int(input("input n: "))
-# makes maps
-maps = [[(i + 1, j + 1) for j in range(n)] for i in range(n)]
+"""
+상.하.좌.우
+* 다시 한 번 구현을 해보았다
+* 조금 더 간결하면서 직관적인 코드를 작성할 수 없을까?
+"""
 
-# get the move
-move = list(map(str, input().split()))
+n = int(input())
 
-# print(f"n: {n}")
-# print(f"maps: {maps}")
-# print(f"move: {move}")
+# travel_map = [[(i + 1, j + 1) for j in range(n) for i in range(n)]]
 
-# set the current location
-cur = maps[0][0]
-# print(f"cur: {cur}")
+# print(travel_map)
+moving_plan = list(map(str, input().split()))
+move_type  ['L']
 
-# set the x, y 
-x, y = cur[0], cur[1]
+# starting point is seted to [1, 1]
+row = 1
+col = 1
 
-# let's move!
-for m in move:
-    if m == "R" and y < n:
-        y += 1
-    elif m == "L" and y > 1:
-        y -= 1
-    elif m == "U" and x > 1 :
-        x -= 1
-    elif m == "D" and x < n:
-        x += 1
+for m in moves:
+    if m == 'U':
+        next_row = row - 1
+        next_col = col + 0
+    elif m == 'D':
+        next_row = row + 1
+        next_col = col + 0
+    elif m == 'L':
+        next_row = row + 0
+        next_col = col - 1
+    elif m == 'R':
+        next_row = row + 0
+        next_col = col + 1
+    
+    # traveler must be in the travel_map
+    if next_row >= 1 and next_row <= n and next_col >=1 and next_col <= n:
+        # update row and col
+        row = next_row
+        col = next_col
 
-# print results
-print(x, y)
+print(row, col)
